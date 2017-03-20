acd0cf2a09510f80ca126ce88b17aa0c0daaf2e1
VahanAper <asryanvahandev@gmail.com>
2017-03-20 11:18:27 +0400

    Video 88 (validation added)

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
+
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   render() {
@@ -7,32 +10,61 @@ class PostsNew extends Component {
       handleSubmit,
       fields: { title, categories, content }
     } = this.props;
-    
+
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label htmlFor="title">Title</label>
           <input {...title} className="form-control" type="text" id="title" />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label htmlFor="categories">Categories</label>
           <input {...categories} className="form-control" type="text" id="categories" />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label htmlFor="content">Content</label>
           <textarea {...content} className="form-control" type="text" id="content" />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button className="btn btn-primary" type="submit">Save</button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title.';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter categories.';
+  }
+  if (!values.content) {
+    errors.content = 'Enter the content.';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
